b224887f76ce4057c1169dd0e71666ac30e07c6b
Fayecodeur <fayecodeur99@gmail.com>
2025-08-12 18:40:15 +0000

    affichage des caracteres

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,34 @@ import Footer from "./components/Footer.jsx";
 import Navbar from "./components/Navbar.jsx";
 
 function App() {
+  const allCharacters = [
+    {
+      name: "Chevalier",
+      image: "chevalier.jpg",
+      health: 50,
+      magic: 45,
+      power: 100,
+    },
+    {
+      name: "Sorcier",
+      image: "sorcier.jpg",
+      health: 80,
+      magic: 100,
+      power: 90,
+    },
+  ];
+
   return (
     <>
       <Navbar />
-      <main className="flex flex-col h-screen grow">
-        <Card />
+      <main className="container mx-auto px-6 py-10">
+        <div className=" flex flex-wrap justify-center gap-4">
+          {allCharacters.map((character, index) => (
+            <Card key={index} character={character} />
+          ))}
+        </div>
       </main>
+
       <Footer />
     </>
   );
