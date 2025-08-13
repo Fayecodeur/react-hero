e19e56bcac52f84c258b1311befe53d80f44a279
Fayecodeur <fayecodeur99@gmail.com>
2025-08-13 17:53:26 +0000

    Ajout des liens dans la barre de navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Navbar() {
+  const links = [
+    { name: "Acceuil", path: "/" },
+    { name: "Combattants", path: "/personnages" },
+  ];
   return (
     <nav className="py-4 shadow px-4">
-      <div className="container mx-auto">
-        <Link href="" className="text-xl text-gray-500">
-          Combattants
+      <div className="container mx-auto flex justify-between">
+        <Link to="/" className="text-xl text-gray-500">
+          La taverne
         </Link>
+        <div className="flex gap-3">
+          {links.map((link, index) => (
+            <NavLink
+              to={link.path}
+              key={index}
+              className={({ isActive }) =>
+                `${isActive ? "font-bold underline" : ""}`
+              }
+            >
+              {link.name}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </nav>
   );
