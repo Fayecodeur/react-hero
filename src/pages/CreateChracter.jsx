bb9f24b3d0afbf6d17e913a7c155f9f1e4a59b5c
Fayecodeur <fayecodeur99@gmail.com>
2025-08-14 18:58:07 +0000

    création du formulaire pour l'ajout de personnages

--- src/pages/CreateChracter.jsx
+++ src/pages/CreateChracter.jsx
@@ -1,77 +1,76 @@
 import { useState } from "react";
+import SidesInput from "../components/formComponents/SidesInput.jsx";
+import ImagesInput from "../components/formComponents/ImagesInput.jsx";
+import GenericInput from "../components/formComponents/GenericInput.jsx";
 
 export default function CreateChracter() {
   const [name, setName] = useState("");
   const [image, setImage] = useState("");
   const [health, setHealth] = useState("");
   const [magic, setMagic] = useState("");
+  const [power, setPower] = useState("");
   const [side, setSide] = useState("");
+
+  function handleSubmbit(e) {
+    e.preventDefault();
+
+    const newCharacters = {
+      name,
+      image,
+      health: parseInt(health),
+      magic: parseInt(magic),
+      power: parseInt(power),
+      side,
+    };
+
+    setName("");
+    setImage("");
+    setHealth("");
+    setMagic("");
+    setPower("");
+    setSide("");
+
+    console.log(newCharacters);
+  }
+
   return (
     <div>
       <h1 className="text-center text-2xl font-semibold mb-3">
         Ajouter un personnage
       </h1>
-      <form className="max-w-[500px] mx-auto p-4 shadow-md space-y-3">
-        <div>
-          <label htmlFor="name" className="block mb-2">
-            Nom
-          </label>
-          <input
-            type="text"
-            id="name"
-            className="border border-gray-500 w-full px-3 py-1 rounded-md"
-          />
-        </div>
-        <div>
-          <label htmlFor="avatar" className="block mb-2">
-            Avatar
-          </label>
-          <input
-            type="text"
-            id="avatar"
-            className="border border-gray-500 w-full px-3 py-1 rounded-md"
-          />
-        </div>
-        <div>
-          <label htmlFor="sante" className="block mb-2">
-            Santé
-          </label>
-          <input
-            type="number"
-            id="sante"
-            className="border border-gray-500 w-full px-3 py-1 rounded-md"
-          />
-        </div>
-        <div>
-          <label htmlFor="magie" className="block mb-2">
-            Magie
-          </label>
-          <input
-            type="number"
-            id="magie"
-            className="border border-gray-500 w-full px-3 py-1 rounded-md"
-          />
-        </div>
-        <div>
-          <label htmlFor="puissance" className="block mb-2">
-            Puissance
-          </label>
-          <input
-            type="number"
-            id="puissance"
-            className="border border-gray-500 w-full px-3 py-1 rounded-md"
-          />
-        </div>
-        <div>
-          <label htmlFor="side" className="block mb-2">
-            Coté de la force
-          </label>
-          <input
-            type="text"
-            id="side"
-            className="border border-gray-500 w-full px-3 py-1 rounded-md"
-          />
-        </div>
+      <form
+        onSubmit={handleSubmbit}
+        className="max-w-[500px] mx-auto p-4 shadow-md space-y-3"
+      >
+        <GenericInput
+          constante={name}
+          callback={setName}
+          title="Nom"
+          type="text"
+        />
+        <ImagesInput image={image} setImage={setImage} />
+
+        <GenericInput
+          constante={health}
+          callback={setHealth}
+          title="Santé"
+          type="number"
+        />
+        <GenericInput
+          constante={magic}
+          callback={setMagic}
+          title="Magie"
+          type="number"
+        />
+
+        <GenericInput
+          constante={power}
+          callback={setPower}
+          title="Puissance"
+          type="number"
+        />
+
+        <SidesInput side={side} setSide={setSide} />
         <button className="mt-3 bg-blue-500 text-white font-bold px-3 py-2 w-full rounded-md">
           Créer le personnage
         </button>
