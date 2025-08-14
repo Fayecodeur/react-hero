17365cda640b504bc21b11a1915a19f7e9095d00
Fayecodeur <fayecodeur99@gmail.com>
2025-08-14 16:23:27 +0000

    Création de la page d'ajout de personnage

---
+++ src/pages/CreateChracter.jsx
@@ -0,0 +1,9 @@
+export default function CreateChracter() {
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-semibold">
+        Ajouter un personnage
+      </h1>
+    </div>
+  );
+}
